7c6f2d7280aa22be44bc3921d9113256fffa1461
Jabrail <j.shahbazov@tayqatech.com>
2022-12-22 13:19:18 +0400

    add delegate

--- ECommerceApp/EcommerceApi/Infrastructure/EcommerceApi.Infrastructure/Services/Storage/Storage.cs
+++ ECommerceApp/EcommerceApi/Infrastructure/EcommerceApi.Infrastructure/Services/Storage/Storage.cs
@@ -1,19 +1,12 @@
-﻿
-using Ecommerce.Infrastructure.Operations;
-using Microsoft.AspNetCore.Hosting;
+﻿using Ecommerce.Infrastructure.Operations;
 
-namespace Ecommerce.Infrastructure.Services;
+namespace Ecommerce.Infrastructure.Services.Storage;
 
-public class FileService 
+public class Storage
 {
-    readonly IWebHostEnvironment _webHostEnvironment;
+    protected delegate bool HasFile(string source, string fileName);
 
-    public FileService(IWebHostEnvironment webHostEnvironment)
-    {
-        _webHostEnvironment = webHostEnvironment;
-    }
-
-    async Task<string> FileRenameAsync(string path, string fileName, bool first = true)
+    protected async Task<string> FileRenameAsync(string source, string fileName, HasFile hasFileMethod, bool first = true)
     {
         string newFileName = await Task.Run(async () =>
         {
@@ -55,37 +48,12 @@ public class FileService
                 }
             }
 
-            if (File.Exists($"{path}\\{newFileName}"))
-                return await FileRenameAsync(path, newFileName, false);
+            if (hasFileMethod(source, newFileName))
+                return await FileRenameAsync(source, newFileName, hasFileMethod, false);
             else
                 return newFileName;
         });
 
         return newFileName;
     }
-
-    // public async Task<List<(string fileName, string path)>> UploadAsync(string path, IFormFileCollection files)
-    // {
-    //     string uploadPath = Path.Combine(_webHostEnvironment.WebRootPath, path);
-    //     if (!Directory.Exists(uploadPath))
-    //         Directory.CreateDirectory(uploadPath);
-    //
-    //     List<(string fileName, string path)> datas = new();
-    //     List<bool> results = new();
-    //     foreach (IFormFile file in files)
-    //     {
-    //         string fileNewName = await FileRenameAsync(uploadPath, file.FileName);
-    //
-    //         bool result = await CopyFileAsync($"{uploadPath}\\{fileNewName}", file);
-    //         datas.Add((fileNewName, $"{path}\\{fileNewName}"));
-    //         results.Add(result);
-    //     }
-    //
-    //     if (results.TrueForAll(r => r.Equals(true)))
-    //         return datas;
-    //
-    //     return null;
-    //
-    //     //todo: Custom EX
-    // }
 }
